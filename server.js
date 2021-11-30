bd07a4b18181ddf861f4a5f07407e987cae89495
Hoàng Tiến Thành <hoangtienthanh1999@gmail.com>
2021-11-30 12:40:23 +0700

    Test server

--- server.js
+++ server.js
@@ -89,97 +89,97 @@ const server = app.listen(app.get("port"), () => {
 console.log(`server`, `http://localhost:${app.get("port")}`.blue);
 
 // chat real time
-const io = socketio(server, {
-    cors: {
-        // origin: "http://localhost:3000",
-        origin: "https://clothes-store-99.vercel.app/",
-    },
-});
+// const io = socketio(server, {
+//     cors: {
+//         // origin: "http://localhost:3000",
+//         origin: "https://clothes-store-99.vercel.app/",
+//     },
+// });
 
-let users = [];
-let conversations = [];
+// let users = [];
+// let conversations = [];
 
 
-const addConversation = (conversationId, socketId) => {
-    !conversations.some(
-        (conversation) => conversation.conversationId === conversationId
-    ) && conversations.push({ conversationId, socketId });
-};
+// const addConversation = (conversationId, socketId) => {
+//     !conversations.some(
+//         (conversation) => conversation.conversationId === conversationId
+//     ) && conversations.push({ conversationId, socketId });
+// };
 
-const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-};
+// const removeUser = (socketId) => {
+//     users = users.filter((user) => user.socketId !== socketId);
+// };
 
-const removeConversation = (conversationId) => {
-    conversations = conversations.filter(
-        (conversation) => conversation.conversationId !== conversationId
-    );
-};
+// const removeConversation = (conversationId) => {
+//     conversations = conversations.filter(
+//         (conversation) => conversation.conversationId !== conversationId
+//     );
+// };
 
-const getConversation = (conversationId) => {
-    return conversations.find(
-        (conversation) => conversation.conversationId === conversationId
-    );
-};
+// const getConversation = (conversationId) => {
+//     return conversations.find(
+//         (conversation) => conversation.conversationId === conversationId
+//     );
+// };
 
-io.on("connection", (socket) => {
-    console.log(`Client connected`.rainbow);
+// io.on("connection", (socket) => {
+//     console.log(`Client connected`.rainbow);
 
-    //take userId and socketId from user    YES
-    socket.on("waitingRoom", (get) => {
-        const conversation = getConversation(get.conversationId);
-        if (!conversation) {
-            addConversation(get.conversationId, socket.id);
-        }
-        socket.join(get.conversationId);
-        io.emit("getWaitingRoom", conversations);
-    });
+//     //take userId and socketId from user    YES
+//     socket.on("waitingRoom", (get) => {
+//         const conversation = getConversation(get.conversationId);
+//         if (!conversation) {
+//             addConversation(get.conversationId, socket.id);
+//         }
+//         socket.join(get.conversationId);
+//         io.emit("getWaitingRoom", conversations);
+//     });
 
-    // leave room   YES
-    socket.on("leaveRoom", ({ conversationId, senderId, receiverId, text }) => {
-        socket.leave(conversationId);
-        socket.to(conversationId).emit("receive_message", {
-            receiverId,
-            senderId: "Waiting",
-            text,
-        });
-    });
+//     // leave room   YES
+//     socket.on("leaveRoom", ({ conversationId, senderId, receiverId, text }) => {
+//         socket.leave(conversationId);
+//         socket.to(conversationId).emit("receive_message", {
+//             receiverId,
+//             senderId: "Waiting",
+//             text,
+//         });
+//     });
 
-    // customer leave room   YES
-    socket.on("customer_leaveRoom", ({ conversationId }) => {
-        socket.leave(conversationId);
-        io.socketsLeave(conversationId);
-    });
+//     // customer leave room   YES
+//     socket.on("customer_leaveRoom", ({ conversationId }) => {
+//         socket.leave(conversationId);
+//         io.socketsLeave(conversationId);
+//     });
 
-    //send and get message              YES
-    socket.on("sendMessage", ({ conversationId, senderId, receiverId, text }) => {
-        socket.to(conversationId).emit("receive_message", {
-            receiverId,
-            senderId,
-            text,
-        });
-    });
+//     //send and get message              YES
+//     socket.on("sendMessage", ({ conversationId, senderId, receiverId, text }) => {
+//         socket.to(conversationId).emit("receive_message", {
+//             receiverId,
+//             senderId,
+//             text,
+//         });
+//     });
 
-    //sp send and get message              YES
-    socket.on(
-        "spSendMessage",
-        ({ conversationId, senderId, receiverId, text }) => {
-            removeConversation(conversationId);
-            io.emit("getWaitingRoom", conversations);
-            io.emit("removeConversation", conversationId);
+//     //sp send and get message              YES
+//     socket.on(
+//         "spSendMessage",
+//         ({ conversationId, senderId, receiverId, text }) => {
+//             removeConversation(conversationId);
+//             io.emit("getWaitingRoom", conversations);
+//             io.emit("removeConversation", conversationId);
 
-            socket.join(conversationId);
-            socket.to(conversationId).emit("getMessageSP", {
-                receiverId,
-                senderId,
-                text,
-            });
-        }
-    );
+//             socket.join(conversationId);
+//             socket.to(conversationId).emit("getMessageSP", {
+//                 receiverId,
+//                 senderId,
+//                 text,
+//             });
+//         }
+//     );
 
-    //when disconnect
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-        removeUser(socket.id);
-    });
-});
+//     //when disconnect
+//     socket.on("disconnect", () => {
+//         console.log("a user disconnected!");
+//         removeUser(socket.id);
+//     });
+// });
